e6fb63ef878dd9ed8558ada49ba1f6226b19498e
7z <1812287263@qq.com>
2024-06-07 11:03:33 +0800

    目录调整

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// pinia
+import pinia from './stores'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
---
+++ src/stores/index.js
@@ -0,0 +1,13 @@
+import { createPinia } from 'pinia'
+import piniaPlugins from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia()
+
+pinia.use(piniaPlugins)
+
+export default pinia
+
+
+
+// 统一导入导出
+export * from './modules/counter'
