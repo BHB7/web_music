c39f3c613de0796fe8df20372bddd684d13a1d51
7z <1812287263@qq.com>
2024-07-06 16:17:10 +0800

    推荐页调整

--- src/components/playList/itemBox.vue
+++ src/components/playList/itemBox.vue
@@ -6,12 +6,15 @@ const props = defineProps({
 <template>
   <div class="item-box">
     <div class="cover-box lg:w-60 lg:mr-4 lg:h-60 h-20 w-16 mr-2">
+      <div class="plays">
+        <i class="iconfont icon-play m-0"></i>
+        <p class="count font-thin lg:text-lg text-xs lg:mr-2">{{ item.playCount }}</p>
+      </div>
       <img v-lazy :src="item.picUrl || '@/assets/audio.svg'" alt="" />
     </div>
     <div class="desc lg:w-48 lg:mt-4 h-16 w-20">
       <p class="title font-bold lg:text-xl text-md">{{ item.name || '加载失败..' }}</p>
       <!-- <p class="auth font-thin text-zinc-500 lg:text-lg text-sm">作者暂无</p> -->
-      <div class="plays font-thin text-zinc-500 lg:text-lg text-sm">{{ item.playCount }}</div>
     </div>
   </div>
 </template>
@@ -48,6 +51,21 @@ const props = defineProps({
     border-radius: 10px;
     overflow: hidden;
     // margin-right: 4px;
+    .plays {
+      display: flex;
+      align-items: center;
+      color: rgba(255, 255, 255, 0.9);
+      position: absolute;
+      top: 4px;
+      left: 2px;
+      border-radius: 5px;
+      // padding: 2px 8px;
+      backdrop-filter: blur(10px);
+      background-color: rgba(255, 255, 255, 0.4);
+      .count {
+        color: rgba(255, 255, 255, 0.9);
+      }
+    }
     img {
       width: 100%;
       height: 100%;
