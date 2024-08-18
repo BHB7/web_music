f6f716016a828413860bf069f7a06c6c6a5f0cd9
7z <119857599+BHB7@users.noreply.github.com>
2024-08-19 00:40:38 +0800

    更新 index.vue 项目说明

--- src/views/Introduce/index.vue
+++ src/views/Introduce/index.vue
@@ -7,7 +7,7 @@ viewMsgStore.setCNavTitle('关于')
 </script>
 
 <template>
-  本项目在开发初期阶段，仅供学习交流使用，请勿用于商业用途。 如有问题，请加QQ群：<a
+  本项目在开发初期阶段，仅供学习交流使用，请勿用于商业用途。 后续有时间会推出安卓端的版本；如有问题，请加QQ群：<a
     href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=JnSyK9mDWR_kjSZuNSIQxsoC5-TFnytA&authKey=afFEqhHuwSRvi4mxyLNSojfqNHS1eOvwGDyVavHu9iaeBIZbAhsyC6xlUfWn7ReV&noverify=0&group_code=794173085"
   >
     794173085
